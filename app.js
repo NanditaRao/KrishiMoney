28ae3150fb4090299bd2964aaa38b4ead15843c3
Kuber Kaul <kuberkaul1989@gmail.com>
2014-06-07 18:27:52 -0400

    adding all new changes with brand new login system

--- app.js
+++ app.js
@@ -1,49 +1,34 @@
 
 /**
- * Module dependencies
- * Copyright (c) 2014 Nandita Rao & Kuber Kaul
- */
+	* Node.js KrishiMoney
+	* Copyright (c) 2014 Kuber Kaul/ Nandita Rao
+**/
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
-var path = require('path');
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
 var app = express();
 
+app.configure(function(){
+	app.set('port', 8080);
+	app.set('views', __dirname + '/app/server/views');
+	app.set('view engine', 'jade');
+	app.locals.pretty = true;
+//	app.use(express.favicon());
+//	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'super-duper-secret-secret' }));
+	app.use(express.methodOverride());
+	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+	app.use(express.static(__dirname + '/app/public'));
+});
 
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-require ('./app/server/router')(app);
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/helloworld', routes.helloworld);
-app.get('/userlist', routes.userlist(db));
-app.get('/newuser', routes.newuser);
-app.post('/adduser', routes.adduser(db));
+require('./app/server/router')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+	console.log("KrishiMoney server listening on port " + app.get('port'));
+})
---
+++ app/server/modules/email-settings.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+	
+	host		: 'smtp.gmail.com',
+	user 		: 'KrishiMoney1989@gmail.com',
+	password 	: 'krishimoneynan',
+	sender		: 'KrishiMoney <KrishiMoney1989@gmail.com>'
+	
+}
